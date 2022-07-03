37bcc811a719ff9833d54f3200fc94b1c0f10759
Woowahan-Nyong <rltgjqmduftlagl@gmail.com>
2022-07-03 15:07:36 +0900

    (#01) Escape vs UnEscaped

---
+++ src/test/java/Template/thymleaf/ThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package Template.thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
